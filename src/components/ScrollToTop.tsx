1aaf89d443b1762a4771eb930292831eba7fd96b
Blossom Fernandes <blossom.fernandes2006@gmail.com>
2026-06-16 03:33:51 +0530

    docs: add docstring for ScrollToTop component

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,6 +1,9 @@
 import {useEffect} from 'react';
 import {useLocation} from 'react-router-dom';
-
+/**
+ * Resets the window scroll position to the top
+ * whenever the route pathname changes.
+ */
 function ScrollToTop() {
     const location = useLocation();
     useEffect(() => {
